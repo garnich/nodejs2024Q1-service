a73eca09e8c537624bd02aaa8db4d2f22ecf7195
Andrei Ihnatsiuk <garnich@tut.by>
2024-03-09 22:40:53 +0300

    develop: added constants

---
+++ src/common/constansts.ts
@@ -0,0 +1,17 @@
+const USER_VESION = 1;
+const UUID_VERSION = '4';
+
+const HEADERS = {
+    ACCEPT: 'Accept',
+    APP_JSON: 'application/json',
+};
+  
+
+const EXEPTION_ITEM = {
+    ARTIST: 'artist',
+    ALBUM: 'album',
+    TRACK: 'track',
+    USER: 'user',
+};
+
+export { USER_VESION, UUID_VERSION, HEADERS, EXEPTION_ITEM };
