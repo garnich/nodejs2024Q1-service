60e363775097c5161e3accc4b01f27eadf430aa9
Andrei Ihnatsiuk <garnich@tut.by>
2024-03-10 00:09:49 +0300

    develop: added favorites route

---
+++ src/favorites/favorites.module.ts
@@ -0,0 +1,18 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { FavoritesController } from './favorites.controller';
+import { FavoritesService } from './favorites.service';
+import { ArtistsModule } from 'src/artists/artists.module';
+import { AlbumsModule } from 'src/albums/albums.module';
+import { TracksModule } from 'src/tracks/tracks.module';
+
+@Module({
+  providers: [FavoritesService],
+  imports: [
+    forwardRef(() => TracksModule),
+    forwardRef(() => ArtistsModule),
+    forwardRef(() => AlbumsModule),
+  ],
+  controllers: [FavoritesController],
+  exports: [FavoritesService],
+})
+export class FavoritesModule {}
